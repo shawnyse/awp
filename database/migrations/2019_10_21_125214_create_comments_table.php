79f71624cfcce6eb19d716c9c9d20f782c9a9bd3
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-10-21 14:22:20 +0100

    Add Comment model

---
+++ database/migrations/2019_10_21_125214_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up ()
+    {
+        Schema::create ('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table -> string ('name', 64) -> nullable (false);
+            $table -> text ('comment') -> nullable (false);
+            $table -> integer ('likes');
+
+            $table -> timestamps ();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down ()
+    {
+        Schema::dropIfExists ('comments');
+    }
+}
