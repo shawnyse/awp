9b8a2c1728fff21bc653699235eabe5755cde0ff
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-10-28 15:03:58 +0000

    Add first route to show all comments

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller {
+
+    public function index () {
+
+        $comments = Comment::all ();
+
+        return view ('index') -> with (['comments' => $comments]);
+
+    }
+
+    public function create () {
+        //
+    }
+
+    public function store (Request $request) {
+        //
+    }
+
+    public function show (Comment $comment) {
+        //
+    }
+
+    public function edit (Comment $comment) {
+        //
+    }
+
+    public function update (Request $request, Comment $comment) {
+        //
+    }
+
+    public function destroy (Comment $comment) {
+        //
+    }
+
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Guestbook</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+
+</head>
+<body>
+
+    <div class="container">
+
+        <h1 class="title">Guestbook Comments</h1>
+
+        @if (count ($comments) > 0)
+            <table class="table is-striped is-hoverable">
+                <thead>
+                <tr>
+                    <th>User</th>
+                    <th>Comment</th>
+                    <th>Date</th>
+                    <th>Likes</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                @foreach ($comments as $c)
+
+                    <tr>
+                        <td>{{ $c -> name }}</td>
+                        <td>{{ $c -> comment }}</td>
+                        <td>{{ $c -> created_at -> format ('D jS F') }}</td>
+                        <td>{{ $c -> likes }}</td>
+                    </tr>
+
+                @endforeach
+
+                </tbody>
+            </table>
+
+        @else
+            <div class="notification is-info">
+                <p>
+                    The Guestbook is empty. Why not add a comment?
+                </p>
+            </div>
+        @endif
+
+    </div>
+
+</body>
+</html>
