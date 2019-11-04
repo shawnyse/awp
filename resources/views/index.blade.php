d671a4873b39ae3767e2f43114c624a9958aa5fc
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-11-04 13:49:01 +0000

    Add favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,9 @@
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/png" href="{{ asset ('images/favicon.png') }}" />
+
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
 
 </head>
