bee5fee2c626f0df8f74abf9d15fcc03afab0179
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-11-11 13:24:38 +0000

    Add upvote and downvote behaviours

--- app/Comment.php
+++ app/Comment.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Comment extends Model
-{
-    //
+class Comment extends Model {
+
+    public function upvoteAndSave () {
+
+        $this -> likes += 1;
+
+        $this -> update ();
+
+    }
+
+    public function downvoteAndSave () {
+
+        $this -> likes -= 1;
+
+        $this -> update ();
+
+    }
 }
