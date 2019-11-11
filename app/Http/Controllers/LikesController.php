8abfca3027583581b33a4cc997c15139f189b636
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-11-11 13:25:52 +0000

    Add controller for upvotes

---
+++ app/Http/Controllers/LikesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+
+use Illuminate\Http\Request;
+
+class LikesController extends Controller {
+
+    public function upVote (Comment $comment) {
+
+        $comment -> upvoteAndSave ();
+
+        return redirect () -> action ('CommentController@index');
+
+    }
+}
