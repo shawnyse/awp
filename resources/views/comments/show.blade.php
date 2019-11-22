7cca8c026e65c98fca3aee1972052283f9ad00f8
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-11-22 11:25:01 +0000

    Add app layout, tweak view page to use it

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,16 +1,28 @@
-<table class="table is-striped">
-    <tbody>
-    <tr>
-        <td>Name:</td>
-        <td>{{ $comment -> name }}</td>
-    </tr>
-    <tr>
-        <td>Date:</td>
-        <td>{{ $comment -> updated_at -> format ('l js F') }}</td>
-    </tr>
-    <tr>
-        <td></td>
-        <td>{{ $comment -> comment }}</td>
-    </tr>
-    </tbody>
-</table>
+@extends ('layouts.app')
+
+@section ('page_title')
+    Guestbook | Comment from {{ $comment -> name }}
+@endsection
+
+@section ('page_heading')
+    Comment from {{ $comment -> name }}
+@endsection
+
+@section ('content')
+    <table class="table is-striped">
+        <tbody>
+        <tr>
+            <td>Name:</td>
+            <td>{{ $comment -> name }}</td>
+        </tr>
+        <tr>
+            <td>Date:</td>
+            <td>{{ $comment -> updated_at -> format ('l js F') }}</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>{{ $comment -> comment }}</td>
+        </tr>
+        </tbody>
+    </table>
+@endsection
