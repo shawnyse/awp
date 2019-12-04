59627885209d6c58536d85a8d1e18413e54a6e7f
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-12-04 11:43:33 +0000

    Add basic form for new comment creation

---
+++ resources/views/comments/create.blade.php
@@ -0,0 +1,67 @@
+@extends ('layouts.app')
+
+@section ('page_title')
+    Guestbook | Add a Comment
+@endsection
+
+@section ('page_heading')
+    Add a New Comment
+@endsection
+
+@section ('content')
+
+<div class="box">
+
+    <form action = "{{ route ('comment.store') }}" method="POST">
+
+        <fieldset>
+
+            @csrf
+
+            <div class="field">
+                <label class="label">
+                    User
+                </label>
+                <div class="control">
+                    <input class="input" type="text" name="name" placeholder="Enter User's Name">
+                </div>
+            </div>
+
+            @error ('name')
+            <div class="notification is-warning">
+                <p>
+                    {{ $message }}
+                </p>
+            </div>
+            @enderror
+
+            <div class="field">
+                <label class="label">
+                    Comment
+                </label>
+                <div class="control">
+                    <input class="input" type="text" name="comment" placeholder="Enter the Comment">
+                </div>
+            </div>
+
+            @error ('comment')
+                <div class="notification is-warning">
+                    <p>
+                        {{ $message }}
+                    </p>
+                </div>
+            @enderror
+
+            <div class="field">
+                <button class="button is-primary" type="submit">Add Comment</button>
+            </div>
+
+        </fieldset>
+    </form>
+</div>
+
+<p>
+    <a class="button" href="{{ URL::previous () }}">Back</a>
+</p>
+
+@endsection
