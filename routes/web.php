b7fa8109d03c1a71770df23bc5b85e8c10f51655
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-12-04 11:41:51 +0000

    Add routes for creation dialogue

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,9 @@ Route::get ('/comment/{comment}/delete/', 'CommentController@destroy');
 Route::get ('/comment/{comment}/edit/', 'CommentController@edit');
 Route::post ('/comment/{comment}/edit/', 'CommentController@update');
 
+Route::get ('/add/', 'CommentController@create');
+Route::post ('/add/', 'CommentController@store');
+
+
 Route::get ('/comment/{comment}/like/', 'LikesController@upVote');
 Route::get ('/comment/{comment}/dislike/', 'LikesController@downVote');
