c01701d61a4c4bf55eea28862629742e6c4107ab
shawnyse <shawnyse625@gmail.com>
2020-01-07 04:49:09 +0000

    few change

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index () {
+        $comments = Comment::all ();
+        return view ('index', compact ('comments'));
+    }
+
 }
